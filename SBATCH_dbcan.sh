29430429f757cf0e4dc77c91c5a4aac387f9d955
JZygmuntowicz <160242477+JZygmuntowicz@users.noreply.github.com>
2024-02-17 10:54:53 +0100

    Update SBATCH_dbcan.sh

--- SBATCH_dbcan.sh
+++ SBATCH_dbcan.sh
@@ -3,14 +3,14 @@
 #SBATCH -p cpu
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32GB
-#SBATCH --output=/home/users/jzygmuntowicz/db_output_fotosynt/%A_%a.out
-#SBATCH --error=/home/users/jzygmuntowicz/db_output_fotosynt/%A_%a.err
+#SBATCH --output=/home/users/twoje_imie/db_output_bakterie1/%A_%a.out
+#SBATCH --error=/home/users/jzygmuntowicz/db_output_bakterie1/%A_%a.err
 #SBATCH --array=1-180%10
 #SBATCH -w edi06
 
 CONDA_PREFIX="/home/users/jzygmuntowicz/anaconda3/etc/profile.d/conda.sh"
 
-katalog="/home/users/jzygmuntowicz/bakt_fotosynt"
+katalog="/home/users/twoje_imie/bakterie1"
 
 plik="${SLURM_ARRAY_TASK_ID}.faa"
 
@@ -19,4 +19,4 @@ sciezka="${katalog}/${plik}"
 
 . $CONDA_PREFIX
 conda activate Shire
-run_dbcan $sciezka protein --out_dir /home/users/jzygmuntowicz/output_fotosynt/${SLURM_ARRAY_TASK_ID}_output_fotosynt
+run_dbcan $sciezka protein --out_dir /home/users/jzygmuntowicz/output_bakterie1/${SLURM_ARRAY_TASK_ID}_output_bakterie1
